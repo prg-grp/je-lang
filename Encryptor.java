677c0b08659855279a6b7fb40a1905b24dd97ead
Aditya Oak <ata999@users.noreply.github.com>
2020-11-09 18:05:00 +0100

    script changes

---
+++ Encryptor.java
@@ -0,0 +1,264 @@
+public class Encryptor {
+    private String key;
+    
+    public String encrypt(final String plaintext) {
+        String plaintextE = plaintext;
+        String result = plaintextE + this.key;
+        String result1 = result;
+        return result1;
+    }
+    
+    public Encryptor Encryptor$() {
+        this.jif$init();
+        {  }
+        return this;
+    }
+    
+    public static final String jlc$CompilerVersion$jif = "3.5.0";
+    public static final long jlc$SourceLastModified$jif = 1604939784000L;
+    public static final String jlc$ClassType$jif =
+      ("H4sIAAAAAAAAAL1aC5AUxRnuXeCOO17HyZsDluNADrxbRYHogXIcIIcrXI6H" +
+       "cj7Wudneu+FmZ4aZ2WNBsTAWQjQSQ0AhJQQTqBJE0USiUSGWpQI+MRolphQr" +
+       "VhISRKOlMVaC5v+7ex47O3dqlRWqtnump/+//+fXf/ex/wzpZZlk1AolXWuv" +
+       "NqhVu0BJN0mmRVNNurp6CQwl5S/ueym1bblxMkqKWkhvxVqqWVKaJkiJlLXb" +
+       "dVOxV9ukLLFC6pTiWVtR4wnFsusSpI+sa5ZtSopmWyvJzSSSIGUKjEiarUg2" +
+       "Tc0z9YxNxiYMWKhN1e04zdlxQzKlTJyJEm9qUCXLAk5FbNRh0tsw9U4lRU2b" +
+       "jEmA4GK2KrVSNd4kviXwrS5nkpjDXujHlWOcuXZbJ8e33HN92a96kAEtZICi" +
+       "LbYlW5EbdM0GeVpI3wzNtFLTqk+laKqFDNQoTS2mpiKpyhqYqGstpNxS2jTJ" +
+       "zprUaqaWrnbixHIra4CIuKYzmCB9uUmysq2bjjpFaYWqKeetV1qV2iybDPHM" +
+       "wtWbh+Ngi1IwJzXTkkwdkp4dipZCWwQoXB2rroAJQFqcoeAvd6memgQDpJx7" +
+       "TpW0tvhi21S0NpjaS8/aaOARXTKtQ0dIcofURpM2GRac18Q/wawSZggkscng" +
+       "4DTGCbw0IuAln3/OLJyx6UZtvhZlMqeorKL8vYFodIComaapSTWZcsK+kxJ3" +
+       "S0MObYwSApMHBybzOY/d9PGs80Y/fZTPGRkyZ1HrCirbSXl3a//XKhqqL+7B" +
+       "Q1C3FHR+nuYs+JvEl7qcAYk1xOWIH2udj083P7983T56OkpKG0mRrKvZDMTR" +
+       "QFnPGIpKzcupRk1MkUZSQrVUA/veSIrhOaFolI8uSqctajeSniobKtLZO5go" +
+       "DSzQRMXwrGhp3Xk2JLudPecMQkhv+JGh8Iv4+t42eTDermdoXEpBWktJXeqI" +
+       "17PHeFN7KtlMVRQrfrlit2dbk5BsK2AMjBFfqin8KVmvqjgORrPiVluuBgMr" +
+       "C6FQA3Fg6KZdo2QMNV4bt6ll18iSBVFgmXI8AzgRb3MUr8G8TtG4nU1Jcctu" +
+       "i4NbzdUGpE18rvNUC3MMmzzw/xH4OxE3h/YvWxWJQGhUBIFJhZyer6sAXkl5" +
+       "S3b23I8fSr4YdRNVeM4mJS5HEokwToMwgXmAQXh0ANAAAPetXnzdghs2VvaA" +
+       "yDZW9UQHw9TKPKBv8NCokQGzDCnx+mXGDZumjpwRJb1aALCtOTQtZVW7qWG2" +
+       "ntUA2Aa5Q80UME9jSBuK9sWGzGhsMrQApzk+A5npMUGykZCGVUEwCBNzwIZT" +
+       "/zpw91rdgwWbVBWgVSElok1l0PSmLtMU4LfHflJMOpg8tLYqSnoChIFuNmiG" +
+       "iDg6uEYe6tQ5CI669AL10rqZkVT85Fil1G439VXeCIuJ/tiU8/BAjwYEZOA/" +
+       "c7Gx48Qrf78wSqLePjHAt+8upnadD5uQ2QCGQgO9AFliUgrz3tnW9NOtZzZc" +
+       "w6IDZowLW7AK2wbAJNh9wYLrj67848l3d78R9SLKhq0526oqco7pMvAr+BeB" +
+       "35f4Q4DBAexhm2kQ4BZz0c3AlSd4sgHOqZCFILpVtVTL6CklrUitKsVw/u+A" +
+       "8Rcc/GBTGXe3CiPceCY57+sZeOPDZ5N1L17/+WjGJiLjPuvZz5vGwfscj3O9" +
+       "aUqrUY7cLb8ftf2ItAO2AYBeS1lDGZoSZg/CHHg+s8Vk1sYD36ZgE4OsDX6E" +
+       "5UZ6uclyBEoVhdcxSXnIJ5VxY96c95jvSyEc01CeKTIUXhUFqdXgfsX8wnKh" +
+       "zZk8qmByo/cZM2NoUAaxfs/rYqlPYpXXsHTok6KWbCqGE2SwuZRaCJJgbppi" +
+       "WQxlja0vAPO5NZopaRZiMc/8Jezj3JxhYoXQKZnMT8wqlTkMWFeMJiz9kvL0" +
+       "OzaY+rjbp0WFIfvzgAPTzSCiifh7/HqOge2gHFSnKY5YMUOOqQ7UXIJxzxZy" +
+       "ZPMs7smXlHcMvuep8gc21/MaYUw+RcHsGec33Ja86JGXWZZgFI0OmrSZSoDx" +
+       "3OZJ+ZOdb9PmqV98yLNaX6UF61oDSjJZMSSsbcUTlsQm44J61INUwwpiR7Cf" +
+       "dueuA2febZrFAt7nJSxcCmpnEQYuIPHHOfn7hitP7RLdcEVKytcPOT654qnl" +
+       "P/SbKUDgm71p773FH533xS6mthsr4wKx4hJ0Gy/YXszlZYCS5yC/kH4/DR38" +
+       "zhtHO+d/yMUNxkEYxWVTBh0+NWz4jcKzuOBcsSp2jaHOvgqOR56zY7WJZ35X" +
+       "3PyCz9nMg2CCVWwi9ye2sz0HLALG48PsOVu3bT3js+rMcW+vqDv72qNOlsxz" +
+       "rVKdr2CA0q9m0aQnh2/607pFDo8FXNUmn6rNfOgibKpzLMuWsZGZFkJIoBqZ" +
+       "L1ntsOWcUN9q2frOpNHc4L4tSXx/Ys76rXc//thFvGDpC0lcdtksQjg6IvNZ" +
+       "fDlsr/FEqs4TKWRooUd2g+e0atdphUO8H+YWTRV5RdM8PK55hYK8ZuZfN3+5" +
+       "EgqFHi2kf7tkNWqw++LpEA6hCL/um00G+lKO4RqWC6q/9AkeaQKLtcT33zui" +
+       "4dLTLJu9qgSpx+QKS8plkq9gmrIv81m0sui5KCmGuo5Va3ASXyapWSwIWuBg" +
+       "aTWIwQTpl/c9/5DIT0R1btVVEayIfMsG6yGvlIVnnI3PpQFER9+TyfCDUCAr" +
+       "RH+FH9EjhD2ojGQsa6uwOdcpS4ohSQBQaM5lyrbZPoLZAtE3+JjapEcHXW3l" +
+       "7YEM3WmKnxX33L//obq+e/ew5C1hbgMn2mK/640UzjvXqF++RhPEojRMI55Q" +
+       "LkF5GMH3/QSs6xRZh+3VoTnjcjwHB0cKTi2ib/ZzxHYN6F8VCt0Yrj6kqVAy" +
+       "2d2fvtDEIrEfu59xfG6T4cE494pvhkr5Qg0TwlwVJpRVsOMyhn7AOpE4u3Pm" +
+       "m4cPujvu8ED14OXAtGP7ery67Nod3IW+qt6/GeLByHfLIa5XRnelE79dYYop" +
+       "rmLDUbGJohhx+uF+xQgP4fXhIdwDHydCeW2x2ygbskjRJJWtUg1v9bCfUKsQ" +
+       "L8BDGQCcTnEFQjduuf2r2k1bor57onEFVzV+Gp82/QQommRsd6swinl/O7D2" +
+       "yfvXbuDgXp5/6zFXy2YefPPsS7Xb3jsWdqB1o8w5/lR04UC2VOX7xRsaTy3Y" +
+       "wwC3DEGLlQOAWpiRbKhRyx/qo1hu6DLV7sJmM6I7Q3/XbSPCkmR5QZ6KXWOh" +
+       "SzcK6caL+SnRXxtMrm1s/k35VJVithRGxaau8wWKJ2pV2JJyV6IOhhqY7biI" +
+       "4LX8mo9Zm0dhJ588Fpvx7hIYs6RYPDh9xLeEb59kJ5hRXd3iMcft/sGWnalF" +
+       "ey5wCou7wPW2btSotJOqgS03/0L6SnZv6aXx9PvmVFU8s3LTd3dFIUA87DZi" +
+       "TECpoDB7r9x/7PIJ8maIR3dLLLiLzSeqy98IS/mqS/K2w9GuG3DTIjXw6wW/" +
+       "7aJfFQyusjAPMs+UCpJO0RtBD3pH1YhwAL5bLpfvIZdZgnq/6HcHUHpEsPKt" +
+       "N9vE4fH+fs+9eGbYvKPs8BiVFTyHFlzUpGhX1skaBjX9Xop2KvjUwfSe7op5" +
+       "aZiY+/xi2mQZrpvRTaNdESfBmJ6O8UuamGS2ZTNUg1OiijFCczZ+5JfmMXGj" +
+       "F5vYipLQVExq1TtprHV17Ma11YZbobvbVIOkabpdcDwskpWDZ+Lps04SXMrz" +
+       "cy82D3JHYvNQd47B5tc8Yx/F5jFG9zhT8olvx6+wZlyqdWhwMOH7x+I++7O3" +
+       "Hqo54UgrtgT2/Ew31xzPY7PfxhtrZjV8fZaQsFuPghOTEEDEzqD4BzsW/fvP" +
+       "DzsSTOeKifPXb3l3LDCIe4rjwkBh9fVBwrpXv7awwuY4E+l17+RxvPAwcrzw" +
+       "MMLJTnxDsuD5I6HLkurBz5IfHXlr2vZTm9nG2v1hIkCp7laPJD5d/YpjWiU/" +
+       "4RcI4/wmLOFFAX6yiwIcH293ipWAL0T4Mitjs6sgkLH5CwvmUPvj2JPYvO/y" +
+       "OMkYsS+HugnLUywssTkSgLg+zm6Kmv4kDGMDcTQwjGBtQRyd/uYFOiv5RwhO" +
+       "t4n+5iDMfxSoIRjVUDH71jCqghqiKb9KDS65voC4OVjuVCBdlZh/l+g3BkX9" +
+       "PCAqoxorZt8ZRtVNuTMubMkfFxA3spddxLtg9ZDOt0PVZ+32BrcYSMqJz/4w" +
+       "pHOi/jy/JXPrUf8FvStJM0rSKiR4RfSHCyQJx27PGtciG0mQvyz6o4XW6DIF" +
+       "1mMb6RkMTCwUyHTB75ei3x4MzEjJNwG4CCtFIv1cWIKBIFIFhrwgqUVe04QA" +
+       "vxD9zwJBEikPmIVRXShm/zyMqpsgmRq25H1dBMkyGzztINO5YVDySNdQEqkI" +
+       "Qgk2/2Bkp7sKQaQj2ES7CBH81ouxH8vaEiZnzv83PyOsnuc3MjmSV0Tn25WZ" +
+       "5hLCa3inn/qtwq0ba0zOvzbBkj3L//dGUv7nlAvmHD464Yi4aHX3JLB6Lbs3" +
+       "cOpql+LAzgULb/x4Gr9o6SWr0po1uGhvKNd5pSby0n8wDXJzeBXNr/5P/4dL" +
+       "xrt/N8DG+UObZy6Sd+cn9M27xRbX6v7/XpKUO8jaO57dUH4LSNpCShRriZm1" +
+       "bPyPHiWyc/RBXrfy0sXgqXchMD43eN/sY+svFyMrti9KFH91tVsuhvqHCVz2" +
+       "P5fDhxLMIwAA");
+    
+    public Encryptor(final jif.lang.Principal jif$Alice) {
+        super();
+        this.jif$Encryptor_Alice = jif$Alice;
+    }
+    
+    public void jif$invokeDefConstructor() { this.Encryptor$(); }
+    
+    private void jif$init() {  }
+    
+    public static boolean jif$Instanceof(final jif.lang.Principal jif$Alice,
+                                         final Object o) {
+        if (o instanceof Encryptor) {
+            Encryptor c = (Encryptor) o;
+            return jif.lang.PrincipalUtil.equivalentTo(c.jif$Encryptor_Alice,
+                                                       jif$Alice);
+        }
+        return false;
+    }
+    
+    public static Encryptor jif$cast$Encryptor(
+      final jif.lang.Principal jif$Alice, final Object o) {
+        if (o == null) return null;
+        if (jif$Instanceof(jif$Alice, o)) return (Encryptor) o;
+        throw new ClassCastException();
+    }
+    
+    private final jif.lang.Principal jif$Encryptor_Alice;
+    public static final String jlc$CompilerVersion$jl = "2.7.1";
+    public static final long jlc$SourceLastModified$jl = 1604939784000L;
+    public static final String jlc$ClassType$jl =
+      ("H4sIAAAAAAAAAL16W8ws2VVenzP38eC5EBtjZoYf+9iacc+c6q5LXxhDXF3d" +
+       "XV3VVV1d9+qyzKGuXdV1v1eXM8hBxLaCZBCMuUhcXowExA5SJJSHCImXJCBQ" +
+       "pERRBA8EHiIlEfEDD8ALhFT1fz3/OT5+Qfml2rV777XXXnutb629a6//m9/u" +
+       "PZOlvbM48o97P8rv58fYyu5vtTSzTMzXskxoGx4YX+8DH/zij73yb57qvaz2" +
+       "XnZDPtdy18CiMLfqXO29FFiBbqUZapqWqfZeDS3L5K3U1Xy3aQmjUO29lrn7" +
+       "UMuL1Mo4K4v8siN8LStiKz3NedlI9V4yojDL08LIozTLe69QB63UgCJ3fYBy" +
+       "s/w9qves7Vq+mSW9n+jdoXrP2L62bwk/Sl2uAjhxBJZde0v+otuKmdqaYV0O" +
+       "edpzQzPv/eDtEVcrvrduCdqhzwVW7kRXUz0dam1D77VzkXwt3AN8nrrhviV9" +
+       "JiraWfLex78j05bo+VgzPG1vPch7H7tNtz3vaqleOKmlG5L3PnKb7MSpTnsf" +
+       "v2WzG9b69uazX/tiuArvnmQ2LcPv5H+mHfTmrUGcZVupFRrW+cCXPkP9gvbR" +
+       "3/vq3V6vJf7ILeJzmn/7z/7qc++8+ft/cE7zA4+hYfSDZeQPjG/oH/7Pr2Nv" +
+       "T5/qxHg+jjK3g8JDKz9ZdXvR814dt1j86BXHrvP+Zefvc/9h96Xftv7ybu9F" +
+       "ovesEflF0KLqVSMKYte3UtwKrVTLLZPovWCFJnbqJ3rPtXXKDa3zVsa2Mysn" +
+       "ek/7p6Zno9PvVkV2y6JT0dNt3Q3t6LIea7lzqtdxr9d7vn1639c+d268n897" +
+       "3wKcKLAAzXTzo/Yg0jwAPVWBrWM+4Cy/EwvA3dwp9AfbNDq0ba0yADF0z2sP" +
+       "UN/v2lulZUC2r9/tgFW0UHi3xUEcpfm7bhD7wH0gt7L8XUPLWhRkqQEEmhsC" +
+       "+8uFv3twbcC0gLwwNSDL90Br1vQYt04ELC5r91uaOO/9q/8/Av+jiFt3+v+e" +
+       "6s6dFhqv3w5TfuvTq8g3rfSB8UExW/zVv37wR3evHPXCcnnvhSuOvTt3Tpz+" +
+       "SefA5wBr4eG1YaeNLC+9zX+B/PGvfuKpFtlx9XRn4Jb03m0/u45ORFvTWud5" +
+       "YLz8lf/1N7/zC+9H1x6X9+49EggeHdk58iduryqNDMtsA+U1+8+cab/74Pfe" +
+       "v3e3Q+ULbXzMW112webN23M85NDvXQbHThN3qd6H7CgNNL/ruoxoL+ZOGlXX" +
+       "LSd1f+hU//A/tH932uf/dk+H/a6he7cRELvwu7Mrx4vjc1N12r21olMg/hE+" +
+       "/tU/+U//G7rbSXIZs1++Edx5K3/vRpzomL10igivXhtLSC2rpfuzX9r+/Ne/" +
+       "/ZXPnyzVUnzycRPe68pOTq2VL0r/xR8kf/rn//0b//XutXXz3rNxofuucZL8" +
+       "9ZbRp6+nakOI3wK8lSS7J4ZBZLq2q+m+1SHl717+1PB3/8/XXjk3t9+2nCsv" +
+       "7b3z3Rlct3//rPelP/qxv33zxOaO0W1h1+q4JjuPi997zRlNU+3YyVH/8//y" +
+       "xi//R+1X2wjbRrXMbaxToLpzAd5OqNdaY7VudL9z0RZabmi4seafFtw/EXz6" +
+       "VH6ms+tpWO/UB3TFD9Snvo+c2rtzwu3tY9ntw9cwVYFv/srHsR/9y9N6rmHa" +
+       "8fh4/aj7StoNDwJ/O/jru5949t/f7T2n9l45HQG0MJc0v+gMrrabeIZdNFK9" +
+       "73mo/+EN+Xz3ee/KDV+/7SI3pr3tINdho6131F39uZs+0SripU5J/fZp0dc7" +
+       "XLzXXe8rcVe+Wt/pnSrIacibp/KHuuLeJeyei1O3bJ2mvmJ60vyHLpiRF2/s" +
+       "BtO895RnHS9N+soJDCebnh9Azl2vK4cnLNd32mmege4j9wfd7x99vChPddVP" +
+       "tW6Qnc5z7QjbDS/Q8Xre+76Db9y79HSpPd+1YLzXgqnr/uxjZmwR8uFrwaio" +
+       "PRf99P/42T/+mU/+eYsIsvdM2VmrBcIN6TdFd3D88je//saHPviLnz75Zquh" +
+       "4lt/9uuf67guuuJz7aGqk4SPitSwKC3L6ZMzWWYnzGNg2cI8aGNIeXGqsb76" +
+       "wb/8h/tf++DujaPfJx85fd0cc378O6nhxfPFtbP80JNmOY1Y/s/fef/f/eb7" +
+       "Xzk/Gr328EFmERbBt/7b3//x/V/6iz98zB71tB893oqvCSs4I9DLP2qoYQgq" +
+       "1lxYKBbGMMPx6qhmc7/YoPO01rndKtorhEEQRqoQo5VxGMuFGVpFfxmosKaC" +
+       "8maWCajMHlieHaAOybJCMiMXA5F3RR6XY5bo01zkxw6bSg7Bc2vGp6V2yrI/" +
+       "tmp4zEBm4nNykreriYE+APRtxQasGqiaWFV9b4hGviIX5SZxpGIwIzeDIOCk" +
+       "zaZI4xHGVj7CA4V1DIa67te0l6xlzylrKUlkaa2Rnkv6lKKbElNL08KjqCU2" +
+       "igVXlnIOrsJlVmR8TPQzDVFzPooD+ViSDc/zBC6uDwtXS8U9KBiDZrqrN7W7" +
+       "o+W1sxlaA7gx/Zpt1r5AHslFME1ETKFQD1nqG84Z5Fk1FWGfE0H9sAhQGF+D" +
+       "61TiNybsla5SpTwSevFhrhSHlS8zRqGoOjmxIGi1mYLadOShi1wlMJfSd76M" +
+       "LHSTHuCWFBNQ1WzyJSUkXt/X1slyvmEjgQdbV1rHRyfZJXiWx5uaBw8C2cSp" +
+       "qCWanIa6W6+IVIMxoljvmgCbTQsxaJ2+le0QL7WdnKwtK5eGiQS6R32+XdFV" +
+       "LutOf7N3h0RFNorgeZXpxbDpxiomV2ykzMA0jVCSXSZGZKGlOWo/1fbsPCbr" +
+       "sRDLg7x0XHDIDzbxQeMngKYxKKcI65wdmIQR2SIX0yDOacZwwgKVVvHqfu9l" +
+       "RriH5wKO6dWAKFA/hACGIo/1SFRbdqpPJ1aUrqlJuVtGJtHGD02LeUFWjWzd" +
+       "92oS4gc0o+8qHo3p1eyoN8wOMKdbPJsMdoCtCluc847qcJVibqrzig9pAKCs" +
+       "nSSh+uMZO8Sg5Yrrh8Uu65Plwh8nBrrzhjUjEyuwMA+DCbQdTyeQgIwYL17j" +
+       "g4BL16U6pwWOXYZbnIjXjVHEnDekPVHlW5SLKFBaPJE6jLaMfGJS8ImRaweL" +
+       "D91IX+eUyiD7aM43LEe3eMCdGFI8kKQKc2sZNNvit0/izoQo4X4Og5Ywt9LA" +
+       "jJRFPJO9nFSshBiOtytruRmv89aVODa1U9/NQVP3kFm2BlH6MFvaiRtO1lJQ" +
+       "Joc9w6VbV/KLYranU2y0ZFFouR+tVTMCWXojwXo+8v3hxOVn0nIfKMQEbfac" +
+       "DtFiOsEnQo7DoTgkDSMvXHijN6aIx4vhJC03IgbRdNA0FVCvYKZeQA6wK+Va" +
+       "JlncEWfEknTUo5WOMlTxWjeFdBAiJjafwjuT20gWDQckj6wWkVcJEV3tlQBl" +
+       "FWeAbtqYtLVyJWzMvZOBUqmhx7kqT0V2VU2F9XY9KycbLPJYvJljYd0PFHyu" +
+       "FsEhrjhSH2ooheWMwOaqJI8TGQfDXERBVEPYyUCkF9MDPyvW00zGG9zJ6JRF" +
+       "48bro0gY0yJ+9FQMl8RFhc02TUWoAtDE1XSYmvtK1qWZvJf4hR95Q3+w5QMq" +
+       "p0WYsle2T9JMvJ/k1C7hwwhziGWxCVzcne9HxWJnjXIWwYjRaNoG2kGaqJ4k" +
+       "TJCl6w9xGJpKbcQlChLIXXECzNBDtVuoDAoYY7yGJ61dx5OKTyhre1iheX9S" +
+       "xKFmzpGCBVkMZ/fMFF36rrhPGygP5emGydVGNaoRMVejipLH6BqsdJTcONNq" +
+       "R6WH0XgCT3zIHE+4KU+18B8g4ChqnXgc0yWa9mfWDEJsmIXnM9SqnQD0WHim" +
+       "NgMDQn3ehLP9iklUbElQsZRLAjXCPD8V3XydSRVQ9NEJZxgAPhNwxYIwd2tY" +
+       "2A4Z9zPVYrZzFdkMqeEY6MsHnHSpw7Yo1QYp8crOjAPETDUxScXd2KD0iZ3b" +
+       "IJGjDIjuZhY/9Oe4GrUxAsBWq30/m0jeMNvNoZG6X8BMNldGC0RUR4XhH2eL" +
+       "SHJMo+aVkCARqd5NR8YUOZT8cWEMhpi1FghRccRlYip7O1MmVbh2GtWbJMHs" +
+       "GAGGSoObSES12s6AdJf4212sKNlKiLzRWtaxBF+AfFWonIaR5F6PY1dma/+g" +
+       "DUp0yyAbNYo1TnVpA5iQVnOAyNrZRQq7VkNamFSJL3i0vLRo0m2mXkkNmEbb" +
+       "9dODHg/kJWGXzR7dCg2Pua7tjIFVwuwtcL9a8uF6h+xokWESdiwqBFk3tXpU" +
+       "t3Q2zgLFXxl0WVjxZG+AQ4DWdmLV54xVhlsGBJHaZETiiqyq+yUxYIKUV/lo" +
+       "XkOMSvZVxtSn2yCRppMjtdGUkbgcDdtwdCBVmucz3RAFP2MDGCZHQ2IjVlXs" +
+       "cmPMNgurQEHb0tV0iebjkORrAwvicMHuCmK7WjF2OHbcfDvmKNNYSOtjbmwZ" +
+       "YYTASehvAV5fi/N9MB7G43EbbhEIqvlRkDtZClfrGnbIAX4EdkrW+OoO45Ej" +
+       "BngrGJhz0KRx8hXTN4EjLW5qb7GrZUmYOYP+Rm7gg7FuCmZmtLs4eNBWTLlP" +
+       "GA4ZNbuxTKoQokDbI9ZMQ3oPYpgkV+70aIFZM1hRKIXDk23GSAWkC9Ah66e0" +
+       "IffLZF6OwWTD+FndpNLaTEEMHSTTYKCxoQMCo8E4Xs6jyVLZiEE4Sjc2s3f1" +
+       "ddxQdCFnKq4uPZKIZolKDcM8Y2RD3EwT0E6ZeLMyYArOeaUGdtl4lCoGAOTO" +
+       "ssTnMTbwDTP3MvawHxHlRrXnfpYCy0WSwcJa5qJINMF93xpouRk7IjNZ9Xey" +
+       "wEkIZ9KT7RpBnN16ohaqvYH6yGKfLFcu6Nnj3NvISdXvU/ECHTkYBRTBYID3" +
+       "/TUDc4mMjRJginqWxq6p3BAgqGlDu0kJFFlMkz0f1312zU9i30um+yAlyskw" +
+       "GGSFi6yxAC8iRNIglFeOWizzFSG3u4omyETFCrEPGFMAmRPCgBvquSQttCNh" +
+       "0xk+OuKlT0hhSEDbmF0uuGlfONrYXMTQlFv1l6vp4uDMweiAs5k2KirGNGIe" +
+       "jhyenbEajfjHVFe3EaWi+QLqkwctXhgrRYb88W5BQ3KwDmsFgigAiPSkbwFW" +
+       "hWBDR9/TIRisgRE8EJtDqSTLfp9ztovowNEEgrCUt0RwegUBLJ+Nak51+pAU" +
+       "CeHBOFSr2cBYCy7o6oDa1KvdZrScSFg+2LVB1Esk9AiaoF3m8wWRLuwCmdY2" +
+       "PhJgYwFP4bh0vBRh9wOh8iuJjDkpY+ijE45RJl7ABbQNAtjYFoeymCzmQ5hR" +
+       "0Bzfjq30OBpvIhMi5H6SjwDcombHlU0RGqJwoeUvxlNzt4viNKDmoo6TkePU" +
+       "/WLZr9n9FrYVhzCr+epoYpYM1uBxr2fegSNJORf3w3KqY7a4zlhqMpvjLGYI" +
+       "4WLJHsyisMEU9IZ6lCLjqG2TGqmPxxMQYNmoPWFn5iir3NAHmPEcPNqlohfz" +
+       "vRIjwKhZKkdwZ9nbbcaPFb31HtlMZsNoUiGAzw1gEQMlRhnXXKYJR2hXVRg0" +
+       "X+CUzzKo3IYSfe1wxxpIoqFTjOaUNM7IgB4RYxYCFJ3fYl7AIaw6Wisy4/CN" +
+       "sR4ZGtXnQ06pczoNhhngQYhESm7ucHitacWkktckGEsgSR6lEcE47Vl4rLsE" +
+       "TCmTIJqqwcB0SGQyWcZlPXaIXTbidtu5M60pa6EjU49EHE4xoTZ22zmQ4XL7" +
+       "tbq0jkiFRcW42sqg3n4Z5CTTusUAAgbUiA/VcqQp9XRbBtJSnGQQJZiQuSRE" +
+       "dDKeq66wwOJoPhun0xoYbwlyEmgEB5kURblUw1vJcToheCBIeKDdnqd4gUNz" +
+       "gTgo1aiSgbFG2kv2WGQHtI3yfRGLSWbA5bM5FK+GO6dCKUSnYrskVFPdhwnQ" +
+       "fo2txFW8yLbrfDMtF7C96ttZf2rh+q5cb7PJzJWWwnG19Bsxxh1lmHna7KBM" +
+       "lSgG1+VBrGzeGkcxoqnurg/ULhiwahqXnKBtzAVvzwJyxEzEQgtwHlSFTQsn" +
+       "xnUPS8xo1D5C2gGt4IclUu7cmGKlRZ0MhTxOV9JwTGvD4U7VI73UJYLxND02" +
+       "jXmVebE8V9uYlA1gXqHTxjhOwDlErnblhjXh8QzJjcmuBpbIYYlPG0eYRH5Z" +
+       "j/qV6h8TU3YqmlmN6z7R77dfjaskQuD28JPvBRBYVNCwbra78daunTxVD9LE" +
+       "VjU2WxbbvcMIi8V80SRzeNZkMmCX23mT8QJjrV172wLYh6g5Rlhweqi8mTO1" +
+       "pVmVxko51qqq2MGLvj8c97m63KKVkTYVTVOkENbzrJgj7ecw5RXprEEIe6X4" +
+       "KViqQjANcbAcOoSHxWNf25JzxAR28UF2RQDmDAIGqoBilktUGR3dijBW4BRy" +
+       "YxmlBgJMu/khG2V7j1ohWFUG4rY8gOR2VzLAkaG9Xcb3V8U+pMRjlWWCx++p" +
+       "fb8JRuughL05Ts8Nga0O+CxnuaMJFLsl45YIVzlMDod9xJ3Tds1HBHeQxPlq" +
+       "PDxgyd5J3LU+Xc/5EVJGGMxIh0LCZqavOoDf30MzT5luMTSJ6ihV84nK5dKm" +
+       "EOhCpz17WaNQFptmTeX7qTpn8sFWUvsQOEuNHU8nZAPAVbxiXOBwBEhlm4r2" +
+       "ZFnyOUkPUn7tcjBn8yvsyJI1ON7iueG231VseWxGEazIozmxHAa0PuI8WsAq" +
+       "8bgSZmPEcqEappvFgdxUOIxh+ZIoh4ZMrNtPDWLppNU+EaX265xFy3o+bSpy" +
+       "tnC9bUBInMI7AdAc6STO9gGXgMhqaM5o74g3TU5sGGQnzY/tBzq7SDyC3Xsg" +
+       "AM68eJytWY09gAt+jM1Wk2FU1KtJqS1cNWzK9pvwyGRhYUD0IeClgRCCiKwi" +
+       "DoVpkBuGQJJXyUDXBg1TNvrMO3S7k4mL5Tj00TJYzeERvHNhbaVAJDUbUXoG" +
+       "GUyc+H2q/fQSKR2oYTydRiNeTgdxNCxwp1xvTCTm9eMgnFRzhWysmbJJAWQL" +
+       "tBt3nebAZjLH+yKjq8cRb/aNcEqDbQzWlVE5BpR43Z4cDhqDjE1xBVP8JsZ4" +
+       "h0tdNRjq2+N25cpHA+BmSmofB5myTxTDoecwZG2AbNfPyuWRTnN3um436mEu" +
+       "ZYVTJitMasAtYs+dmqKJCkW76yX24nLt1dM131Xq8ztf8J1fawqPv0u821Wn" +
+       "XYFd3h9+b8vq3lXu5QHqu8bpwuudE+vzy883u+ITV/egXUqt99xF5fJ958Y9" +
+       "6I0b6V53K/fGd0qDnm7kvvGTH/yayfzG8O7FtfYm772QR/G7vlVa/g1Wz7ac" +
+       "fvAWJ/qU+r2+of4t+pt/iH/a+Lm7vaeuLpcfySA/POi9h6+UX0ytvEhD4aGL" +
+       "5e+/Wnt3/dt7t7tqb59fvnhXNy+Wr01yS20ndbx4MaS8eMe31XZ91X+RITjZ" +
+       "+cTVfkIu4JQC1fIumXoy5WnUifLBlQCf7V0Ud26+H5G9K86eMFXwhL5TVvaQ" +
+       "9168QtS9a31cS9KBpvfpSyhdvvPe5x9NWf3wWVJomZsUUW69dZ4JOisj1zzr" +
+       "cOuGZeRZc8u+kbZ76+2zL+aOm92/FuGtt997/+2r3NdjTHP6e/a2PF1vFsdP" +
+       "WG71hL5jVyR572PfSdDTKPLi6rp70Xnv6W5pt5R1Smm/+qiy/ul3U9Z5/uKm" +
+       "tty8087Z57/Anz1OIZOHFfLc7Tm73i89USE/9YS+L3fFT+S95y9l6X5/8dZi" +
+       "T5O/8chi7/zwd1nseXLk7AIgehT5lhaeVn3p55H91udPmZOu9YtaoL9/ym6c" +
+       "166Sbqefp3GnSPjO2cUQrdRujzlPYp0PiL6wbBXr2mdvRWfu1YRnVxDstH71" +
+       "48w4+5Gzt677ovfOzoPO2aNJQLFLS1hJ0ZrSt8JciN4y7j8mYr9zJXEL9Qtm" +
+       "tuZn1iPAf+yW8KmumF1kWB+24N1rqncek9k6V8Jp5NeeYPuf64qv5r0PP2yR" +
+       "x7nAcxfGuwmMy6T2xy4BcdnQ9b7xj7fArviZE9WvPGExv9YVv3ies71naFl+" +
+       "bY3zTfPmfzPE8TnT/wd1otLBxiUAAA==");
+}
--- script.sh
+++ script.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo "Enter the Jif compiler directory..."
-read JIFC_PATH
+# echo "Enter the Jif compiler directory..."
+# read JIFC_PATH
 # ~/Softwares/Jif/jif-3.5.0/bin/
+export JIFC_PATH=~/Softwares/Jif/jif-3.5.0/bin/
 
-echo "Enter the Jif compiled principles directory..."
-echo JIFC_PRINCIPLES_PATH
+# echo "Enter the Jif compiled principles directory..."
+# echo JIFC_PRINCIPLES_PATH
 # ~/Softwares/Jif/jif-3.5.0/tests/jif/principals/
-read JIFC_PRINCIPLES_PATH
+# read JIFC_PRINCIPLES_PATH
+export JIFC_PRINCIPLES_PATH=~/Softwares/Jif/jif-3.5.0/tests/jif/principals/
 
-JE_PATH="./test-cases/src/main/je/de/tuda/stg/encryptor"
-JIF_PATH="./test-cases/src/main/generated-jif/de/tuda/stg/encryptor"
-JAVA_GENERATED_PATH="./test-cases/src/main/generatedJava/de/tuda/stg/encryptor"
+export JE_PATH="./test-cases/src/main/je/de/tuda/stg/encryptor"
+
+export JIF_PATH="./test-cases/src/main/generated-jif/de/tuda/stg/encryptor"
+
+export JAVA_GENERATED_PATH="./test-cases/src/main/generatedJava/de/tuda/stg/encryptor"
+
+export JAVA_HOME="~/Aditya/Phd_Related/jvms/jdk1.8.0_241"
 
 echo "Je directory = $JE_PATH"
 
@@ -22,7 +28,7 @@ echo "Translating JE files..."
 java -jar ./je-to-jiff-compiler/target/je-to-jiff-compiler-jar-with-dependencies.jar $JE_PATH $JIF_PATH
 
 echo "Compiling using Jif compiler..."
-eval "${JIFC_PATH}jifc" -classpath ${JIFC_PRINCIPLES_PATH} ${JIF_PATH}/*.jif
+eval "${JIFC_PATH}jifc" -classpath ${JIFC_PRINCIPLES_PATH} ${JIF_PATH}/Encryptor.jif
 
 echo "Start the Java application ? Y/N"
 read RUN_JAVA
