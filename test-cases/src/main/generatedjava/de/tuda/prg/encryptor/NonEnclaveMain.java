309e9f12e8731c354a85ba7a708b015ea223020a
Aditya Oak <ata999@users.noreply.github.com>
2020-12-19 04:52:28 +0100

    cache remove generatedJava directory

--- test-cases/src/main/generatedjava/de/tuda/prg/encryptor/NonEnclaveMain.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-
-public class NonEnclaveMain {
-
-    public static void main(String[] args) {
-        String cipher = NonEnclaveRemoteWrapper.encrypt("mystring");
-	System.out.println("Received cipher = "+cipher);
-    }
-}
