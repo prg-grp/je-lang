d012846c9d86072f7a8231ec48fbdef9f658038c
Aditya Oak <ata999@users.noreply.github.com>
2021-01-12 02:14:16 +0100

    semi-working collections, constructor issues

---
+++ test-cases/src/main/je/de/tuda/prg/collection/NonEnclaveMain.java
@@ -0,0 +1,7 @@
+public class NonEnclaveMain {
+
+    public static void main(String[] args) {
+        List<Task> taskList = new ArrayList<Task>();
+        TaskProcessor.process(taskList);
+    }
+}
---
+++ test-cases/src/main/je/de/tuda/prg/collection/Task.java
@@ -0,0 +1,5 @@
+public class Task {
+    public Integer run (Integer input) {
+        return input + 1;
+    }
+}
