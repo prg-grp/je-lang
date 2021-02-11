e5b94b9d570528c4d914e7b3f5fb4b365081599f
Aditya Oak <ata999@github.com>
2021-02-11 20:50:13 +0100

    'checked endorse' for EventProcessor

--- test-cases/src/main/java/de/tuda/prg/eventProcessing/Main.java
+++ test-cases/src/main/java/de/tuda/prg/eventProcessing/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         List<IntEvent> events = EventGenerator.getIntEvents(100);
         List<EncIntEvent> encEvents = Encryptor.encrypt(events);
-        List<EncIntEvent> result = encEvents.stream().filter(FilterNode.filter).collect(Collectors.toList());
+        List<EncIntEvent> result = encEvents.stream().filter(p -> FilterNode.filter(p)).collect(Collectors.toList());
     }
 }
