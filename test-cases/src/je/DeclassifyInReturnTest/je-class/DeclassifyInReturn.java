8ff9dd79291432387b209eb7cf583b3e002c6dd3
robert <r.kubicek@hotmail.com>
2021-10-19 16:36:26 +0200

    Added working JE Files to test cases

--- test-cases/src/je/DeclassifyInReturnTest/je-class/DeclassifyInReturn.java
+++ test-cases/src/je/DeclassifyInReturnTest/je-class/DeclassifyInReturn.java
@@ -4,16 +4,11 @@ public class Encryptor {
     @Secret
     private static String key = "SECRET_KEY";
 
-
     @Gateway
-    public static String encrypt (String plaintext) {
-
+    public static String encrypt(String plaintext) {
         String plaintextE = endorse(plaintext);
-
-        String result = plaintextE+key;
-        
+        String result = plaintextE + key;
         // String resultDeclass = declassify(result);
-
         return declassify(result);
     }
 }
---
+++ test-cases/src/je/battleship/Grid.java
@@ -0,0 +1,22 @@
+package battleship;
+
+@Enclave
+public class Grid {
+
+    @Secret
+    static boolean[][] gridA;
+
+    @Gateway
+    public static boolean applyGuessA(Guess guess) {
+        Guess guessE = endorse(guess);
+        boolean resultE = applyA(guessE);
+        return declassify(resultE);
+    }
+
+    private static boolean applyA(Guess guess) {
+        int x = guess.x;
+        int y = guess.y;
+        boolean b = gridA[x][y];
+        return b;
+    }
+}
